8b26fee3ffd27abaaebd7dcc104981d88e99c761
Stanislav Genov <stasigenov456@gmail.com>
2023-07-05 18:11:15 +0300

    Exam added

---
+++ Fundamentals C#/Exam/Exam/Password Reset/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string input = Console.ReadLine();
+            string command = Console.ReadLine();
+
+
+            while (command != "Done")
+            {
+                if (command == "TakeOdd")
+                {
+                    StringBuilder odd = new StringBuilder();
+
+                    for (int i = 1; i < input.Length; i += 2)
+                    {
+                        odd.Append(input[i]);
+                    }
+
+                    input = odd.ToString();
+
+                    Console.WriteLine(input);
+                }
+                if (command.Contains("Cut"))
+                {
+                    string[] splitted = command.Split();
+                    int index = int.Parse(splitted[1]);
+                    int length = int.Parse(splitted[2]);
+
+                    input = input.Remove(index, length);
+
+
+                    Console.WriteLine(input);
+                }
+                if (command.Contains("Substitute"))
+                {
+                    string[] splitted = command.Split();
+                    string oldstring = splitted[1];
+                    string newstring = splitted[2];
+
+                    if (input.IndexOf(oldstring) < 0)
+                    {
+                        Console.WriteLine("Nothing to replace!");
+                    }
+                    else
+                    {
+                        input = input.Replace(oldstring, newstring);
+                        Console.WriteLine(input);
+                    }
+
+                }
+
+                command = Console.ReadLine();
+            }
+
+            Console.WriteLine($"Your password is: {input}");
+
+
+
+        }
+    }
+}
